6e647594f4769ac61be331c13f34275e9a079c91
GawronDev <kubusgawron@gmail.com>
2023-04-03 14:19:11 +0200

    First commit

---
+++ loops.java
@@ -0,0 +1,17 @@
+class loops {
+    public static void main(String[] args) {
+
+        // Check how the for and while loops work in Java
+        int to_loop = 5;
+        int n = 0;
+        while(to_loop >= n) {
+            System.out.println(n);
+            n++;
+        }
+        
+        String[] cars = {"BMW", "Volvo", "Audi"};
+        for(int i = 0; i < cars.length; i++){
+            
+        }
+    } 
+}
