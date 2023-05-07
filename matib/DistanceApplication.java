2435228bd165d59a3e05ee718e60887d163044c3
GawronDev <kubusgawron@gmail.com>
2023-05-07 23:09:02 +0200

    Get circumference of a polygon

--- matib/DistanceApplication.java
+++ matib/DistanceApplication.java
@@ -7,22 +7,20 @@ public class DistanceApplication {
         // Initiate the utility module
         GeoUtil geoUtil = new GeoUtil();
         // Initialize the points and pass the arguments
-        GeoPoint p1 = new GeoPoint(1, 0);
+        GeoPoint p1 = new GeoPoint(0, 0);
         GeoPoint p2 = new GeoPoint(5, 0);
-        GeoPoint p3 = new GeoPoint(1, 0);
-        GeoPoint p4 = new GeoPoint(10, 0);
+        GeoPoint p3 = new GeoPoint(2.58, 4.3);
+        GeoPoint p4 = new GeoPoint(5, 0);
 
         // Define the points array
         ArrayList<GeoPoint> points = new ArrayList<GeoPoint>();
         points.add(0, p1);
         points.add(1, p2);
         points.add(2, p3);
-        points.add(3, p4);
         
-        GeoPolyline line = new GeoPolyline(points);
+        GeoPolygon polygon = new GeoPolygon(points, "Poly");
+        System.out.println(polygon.getCircumferenceLength());
 
-        System.out.println("To jest dlugosc lini");
-        System.out.println(line.getLineLength());
 
     }
 }
