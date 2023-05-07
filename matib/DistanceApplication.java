34bc250aef65085ab7b5f00a6e0ee8c4e8bd61b9
GawronDev <kubusgawron@gmail.com>
2023-05-07 22:13:27 +0200

    Working on 2nd part

--- matib/DistanceApplication.java
+++ matib/DistanceApplication.java
@@ -7,10 +7,10 @@ public class DistanceApplication {
         // Initiate the utility module
         GeoUtil geoUtil = new GeoUtil();
         // Initialize the points and pass the arguments
-        GeoPoint p1 = new GeoPoint(1, 1);
-        GeoPoint p2 = new GeoPoint(5, 4);
-        GeoPoint p3 = new GeoPoint(1, 2);
-        GeoPoint p4 = new GeoPoint(10, 3);
+        GeoPoint p1 = new GeoPoint(1, 0);
+        GeoPoint p2 = new GeoPoint(5, 0);
+        GeoPoint p3 = new GeoPoint(1, 0);
+        GeoPoint p4 = new GeoPoint(10, 0);
 
         // Define the points array
         ArrayList<GeoPoint> points = new ArrayList<GeoPoint>();
@@ -19,21 +19,10 @@ public class DistanceApplication {
         points.add(2, p3);
         points.add(3, p4);
         
-        BoundingBox boundingBox = new BoundingBox(points);
+        GeoPolyline line = new GeoPolyline(points);
 
-        System.out.println(boundingBox.getWidth());
-        System.out.println(boundingBox.getHeight());
-
-        // First point
-        System.out.println("Point 1 X: " + Double.toString(p1.getX())); 
-        System.out.println("Point 1 Y: " + Double.toString(p1.getY())); 
-
-        // Second point
-        System.out.println("Point 2 X: " + Double.toString(p2.getX())); 
-        System.out.println("Point 2 Y: " + Double.toString(p2.getY())); 
-
-        // Calculate distance
-        double distance = GeoUtil.getDistance(p1, p2);
+        System.out.println("To jest dlugosc lini");
+        System.out.println(line.getLineLength());
 
     }
 }
