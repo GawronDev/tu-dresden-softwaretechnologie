77b1899c7475cd0f1305daecd4cc2d6f3063e1d9
GawronDev <kubusgawron@gmail.com>
2023-05-23 13:58:51 +0200

    New geo excercise

---
+++ matib/GeoField2D.java
@@ -0,0 +1,16 @@
+package matib;
+
+/**
+ * Excercise 2023
+ * Interface for a two dimensional field data structure.
+ */
+public interface GeoField2D {
+    /**
+     * Method to request field value at a given GeoPoint.
+     *
+     * @param p Location to request value from
+     * @return Field value
+     */
+    public double getValue(GeoPoint p);
+                   
+}
